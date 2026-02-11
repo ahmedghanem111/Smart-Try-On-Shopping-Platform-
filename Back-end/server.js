1def3cd90cc8cc24f0da561fb83044aa9457cc2e
Ahmed Mohamed <ahmedmohamedghanem083@gmail.com>
2026-02-11 22:12:12 +0200

    Add Cloudinary upload system and backend updates

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
-const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const colors = require('colors');
 const path = require('path');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const cors = require('cors');
 
-dotenv.config();
 connectDB();
 
 const app = express();
@@ -18,8 +19,9 @@ app.use(cors());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/upload', uploadRoutes);
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // for images & 3d files
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
@@ -27,6 +29,6 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-    PORT,
-    () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  PORT,
+  () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
