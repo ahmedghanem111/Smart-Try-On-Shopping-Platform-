b8e38edb7b3ed3e01aa6920f92a38e477f7d3b68
AlaaAshraf <alaaa123.1907@gmail.com>
2026-06-27 19:16:27 +0300

    Make CORS configuration robust4

--- Back-end/server.js
+++ Back-end/server.js
@@ -51,8 +51,8 @@ const http = require("http").createServer(app);
 const allowedOrigins = [
   "http://localhost:3000",
   "https://fit-me-tau.vercel.app",
-  "https://smart-try-on-shopping-platform-fh82zgpl3.vercel.app/",
-  "https://smart-try-on-shopping-platform.vercel.app/",
+  "https://smart-try-on-shopping-platform-fh82zgpl3.vercel.app",
+  "https://smart-try-on-shopping-platform.vercel.app",
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
